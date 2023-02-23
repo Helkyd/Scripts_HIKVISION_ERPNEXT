d9796dc62a880fc4e85db91ae6d4794c9009d01c
Helkyd <hcesar@gmail.com>
2023-02-23 21:01:49 +0100

    Update lista_usuarios.py
    changes to accept args and get the DEVICEIP to read Employees...

--- Scripts_ZK/lista_usuarios.py
+++ Scripts_ZK/lista_usuarios.py
@@ -1,33 +1,63 @@
+#Last Modified 23-02-2023
 from zk import ZK, const
+import sys
 
-conn = None
-# create ZK instance
-zk = ZK('192.168.8.103', port=4370, timeout=5, password=0, force_udp=False, ommit_ping=False)
-try:
-    # connect to device
-    conn = zk.connect()
-    # disable device, this method ensures no activity on the device while the process is run
-    conn.disable_device()
-    # another commands will be here!
-    # Example: Get All Users
-    users = conn.get_users()
-    for user in users:
-        privilege = 'User'
-        if user.privilege == const.USER_ADMIN:
-            privilege = 'Admin'
-        print ('+ UID #{}'.format(user.uid))
-        print ('  Name       : {}'.format(user.name))
-        print ('  Privilege  : {}'.format(privilege))
-        print ('  Password   : {}'.format(user.password))
-        print ('  Group ID   : {}'.format(user.group_id))
-        print ('  User  ID   : {}'.format(user.user_id))
+def ler_funcionarios(deviceIP):
+    conn = None
+    # create ZK instance
+    zk = ZK(deviceIP, port=4370, timeout=5, password=0, force_udp=False, ommit_ping=False)
+    try:
+        # connect to device
+        conn = zk.connect()
+        # disable device, this method ensures no activity on the device while the process is run
+        conn.disable_device()
+        # another commands will be here!
+        # Example: Get All Users
+        users = conn.get_users()
+        for user in users:
+            privilege = 'User'
+            if user.privilege == const.USER_ADMIN:
+                privilege = 'Admin'
+            print ('+ UID #{}'.format(user.uid))
+            print ('  Name       : {}'.format(user.name))
+            print ('  Privilege  : {}'.format(privilege))
+            print ('  Password   : {}'.format(user.password))
+            print ('  Group ID   : {}'.format(user.group_id))
+            print ('  User  ID   : {}'.format(user.user_id))
 
-    # Test Voice: Say Thank You
-    conn.test_voice()
-    # re-enable device after all commands already executed
-    conn.enable_device()
-except Exception as e:
-    print ("Process terminate : {}".format(e))
-finally:
-    if conn:
-        conn.disconnect()
+        # Test Voice: Say Thank You
+        conn.test_voice()
+        # re-enable device after all commands already executed
+        conn.enable_device()
+    except Exception as e:
+        print ("Process terminate : {}".format(e))
+    finally:
+        if conn:
+            conn.disconnect()
+
+
+def valid_ip(address):
+    try:
+        host_bytes = address.split('.')
+        valid = [int(b) for b in host_bytes]
+        valid = [b for b in valid if b >= 0 and b<=255]
+        return len(host_bytes) == 4 and len(valid) == 4
+    except:
+        return False
+    
+IPvalid = False
+deviceIP = 0
+
+if __name__ == "__main__":
+    print(f"Conta Argumentos: {len(sys.argv)}")
+    for i, arg in enumerate(sys.argv):
+        print(f"Argumento {i:>6}: {arg}")
+        if i == 1:
+            #Check if Valid IP
+            IPvalid = valid_ip(arg)
+            if IPvalid: deviceIP = arg
+            print (IPvalid)
+    if IPvalid:
+        ler_funcionarios(deviceIP)
+        
+
